3212648f24d74eb6008c139dbb7a5f17676715d8
Pol <polescola3000@gmail.com>
2023-04-27 10:44:21 +0200

    weno

--- app/src/main/java/com/example/battleship/SetUpYourShips.kt
+++ app/src/main/java/com/example/battleship/SetUpYourShips.kt
@@ -2,23 +2,27 @@ package com.example.battleship
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.BoxScopeInstance.matchParentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.TextToolbar
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,13 +45,19 @@ class SetUpYourShips : ComponentActivity() {
     fun TableCell(
         text: String,
     ) {
-        Text(
-            text = text,
-            Modifier
-                .border(1.dp, Color.Black)
-                .padding(8.dp)
-                .width(20.dp)
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "huhg",
+            modifier = Modifier.matchParentSize()
+                .clickable {
+                    Toast
+                        .makeText(
+                            applicationContext,
+                            text,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                }
+                .padding(1.dp)
         )
+
     }
 
     @Preview(showBackground = true)
@@ -63,14 +73,16 @@ class SetUpYourShips : ComponentActivity() {
         LazyColumn(
             Modifier
                 .fillMaxSize()
-                .padding(16.dp)) {
+                .padding(16.dp)
+                .horizontalScroll()
+        ) {
             items(10) {
                 val id = it
                 LazyRow(Modifier.fillMaxWidth()) {
                     items(10) {
 
-                    TableCell(text = (count).toString())
-                    count++
+                        TableCell(text = (count).toString())
+                        count++
                     }
                 }
             }
