9397340fae07bfd562764fcc0c4cf24b423d4e3f
Ant <antoniolopez18022002@gmail.com>
2023-04-27 22:32:58 +0200

    grid working

--- app/src/main/java/com/example/battleship/SetUpYourShips.kt
+++ app/src/main/java/com/example/battleship/SetUpYourShips.kt
@@ -1,6 +1,5 @@
 package com.example.battleship
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -9,24 +8,11 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.TextToolbar
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.example.battleship.ui.theme.BattleshipTheme
 
@@ -72,6 +58,7 @@ class SetUpYourShips : ComponentActivity() {
             index to "Item $index"
         }
         // Each cell of a column must have the same weight.
+
         // The LazyColumn will be our table. Notice the use of the weights below
         var count = 0
         LazyColumn(
