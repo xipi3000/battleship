755deb70814fcd472d7b107bdd6cf18cf48aba8c
Ant <antoniolopez18022002@gmail.com>
2023-04-29 00:05:02 +0200

    cells change sprite according to boat size

--- app/src/main/java/com/example/battleship/SetUpYourShips.kt
+++ app/src/main/java/com/example/battleship/SetUpYourShips.kt
@@ -1,7 +1,6 @@
 package com.example.battleship
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
@@ -11,6 +10,9 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -20,11 +22,11 @@ import androidx.compose.ui.unit.dp
 import com.example.battleship.ui.theme.BattleshipTheme
 
 class SetUpYourShips : ComponentActivity() {
-
-    // Calculate the desired dimensions for the image
+    lateinit var lastShip:Ship
+    lateinit var grid:Unit
+    lateinit var isClickedState: SnapshotStateList<Boolean>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             BattleshipTheme {
                 MainView()
@@ -34,34 +36,65 @@ class SetUpYourShips : ComponentActivity() {
 
     @Composable
     fun TableCell(
-        text: String
+        text: String,
+        hasShip:Boolean,
+        onCellClicked: () -> Unit,
     ) {
-        Image(painter = painterResource(id = R.drawable.water), contentDescription = "huhg",
+        Image(
+            painter = painterResource(
+                id = if(hasShip){
+                    R.drawable.saltwater
+                }else{
+                    R.drawable.water
+                }),
+            contentDescription = text,
             contentScale= ContentScale.FillBounds,
             modifier = Modifier
                 .padding(1.dp)
                 .aspectRatio(1f)
                 .fillMaxWidth()
-                .clickable {
-                    Toast
-                        .makeText(
-                            applicationContext,
-                            text,
-                            Toast.LENGTH_SHORT
-                        )
-                        .show()
-                }
+                .clickable {onCellClicked()}
         )
     }
+
+    private fun calculateCoords(start:Int) {
+        //implemented most basic version imaginable
+        /* TODO:
+            - Add ship orientation
+        *       -> [V]ertical - [H]orizontal; 90º rotations alternating state; i have ideas to implement rotations
+        *   - Check if valid position [de moment només canvia la imatge de la cela SEMPRE, encara que IndexOutOfBounds]
+        *       -> Horizontally: ship fits without changing line (ex: ship.size = 3 -> if casella%10 != casella+size%10 no cap)
+                                                                                                [_6=ultima on si cap a cada fila]
+        *       -> Vertically: Ship fits in existing vertical lines (ex: ship.size = 3 -> if casella > (100-(size*10))+9 no cap)
+                                                                                                [79=ultima on si cap]
+        *       -> Non Already Occupied: if(isClickedState[all_cells] = false){putamadre} else {cagaste manin}
+        *       ->Diria que ja no hi ha mes casos en que no hauria de poder
+        *   */
+        val array = arrayListOf<Int>()
+        var size = lastShip.type.size
+        while(size !=0){
+            if (size == lastShip.type.size)array.add(start)
+            else array.add(start + lastShip.type.size-size)
+            size--
+        }
+        lastShip.coords = array
+        for (item in array){
+            isClickedState[item] = !isClickedState[item]
+        }
+    }
+
     @Preview(showBackground = true)
     @Composable
     fun MainView() {
-        lateinit var lastShip:Ship
         val carrier=Ship(ShipType.CARRIER)
         val battleship=Ship(ShipType.BATTLESHIP)
         val crusier=Ship(ShipType.CRUISER)
         val destroyer=Ship(ShipType.DESTROYER)
         val submarine=Ship(ShipType.SUBMARINE)
+        isClickedState = remember { mutableStateListOf() }
+        for (i in 0 until 100) {
+            isClickedState.add(false)
+        }
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxHeight()
@@ -73,40 +106,64 @@ class SetUpYourShips : ComponentActivity() {
                     .aspectRatio(1f)
             )
             {
-                LazyVerticalGrid(
+                grid = LazyVerticalGrid(
                     userScrollEnabled = false,
                     columns = GridCells.Fixed(10),
                     content = {
                         items(100) {
-                            TableCell(it.toString())
+                            TableCell(
+                                text= it.toString(),
+                                hasShip = isClickedState[it],
+                                onCellClicked = {
+                                    calculateCoords(it)
+                                }
+                            )
                         }
                     }
                 )
+
             }
             Row(
                 verticalAlignment = Alignment.Bottom,
-                modifier = Modifier.fillMaxWidth().padding(20.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
             ){
                 Image(painter = painterResource(id = R.drawable.carrier),
                     contentDescription = "Carrier",
                     contentScale= ContentScale.FillHeight,
-                    modifier=Modifier.clickable{lastShip=carrier}.fillMaxWidth().weight(1f))
+                    modifier= Modifier
+                        .clickable { lastShip = carrier }
+                        .fillMaxWidth()
+                        .weight(1f))
                 Image(painter = painterResource(id = R.drawable.battleship),
                     contentDescription = "Battleship",
                     contentScale= ContentScale.FillHeight,
-                    modifier=Modifier.clickable{lastShip=battleship}.fillMaxWidth().weight(1f))
+                    modifier= Modifier
+                        .clickable { lastShip = battleship }
+                        .fillMaxWidth()
+                        .weight(1f))
                 Image(painter = painterResource(id = R.drawable.cruiser),
                     contentDescription = "Cruiser",
                     contentScale= ContentScale.FillHeight,
-                    modifier=Modifier.clickable{lastShip=crusier}.fillMaxWidth().weight(1f))
+                    modifier= Modifier
+                        .clickable { lastShip = crusier }
+                        .fillMaxWidth()
+                        .weight(1f))
                 Image(painter = painterResource(id = R.drawable.destroyer),
                     contentDescription = "Destroyer",
                     contentScale= ContentScale.FillHeight,
-                    modifier=Modifier.clickable{lastShip=destroyer}.fillMaxWidth().weight(1f))
+                    modifier= Modifier
+                        .clickable { lastShip = destroyer }
+                        .fillMaxWidth()
+                        .weight(1f))
                 Image(painter = painterResource(id = R.drawable.submarine),
                     contentDescription = "Submarine",
                     contentScale= ContentScale.FillHeight,
-                    modifier=Modifier.clickable{lastShip=submarine}.fillMaxWidth().weight(1f))
+                    modifier= Modifier
+                        .clickable { lastShip = submarine }
+                        .fillMaxWidth()
+                        .weight(1f))
             }
             Button(onClick = { lastShip.rotate() }) {
                 Text(text="Rotate Ship")
