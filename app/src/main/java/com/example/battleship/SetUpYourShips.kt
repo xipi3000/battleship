ffb9f40a4d2c68facfc910f12ee7746e93387f2e
Pol <polescola3000@gmail.com>
2023-04-28 12:39:56 +0200

    tablero fet dinamicament

--- app/src/main/java/com/example/battleship/SetUpYourShips.kt
+++ app/src/main/java/com/example/battleship/SetUpYourShips.kt
@@ -5,12 +5,19 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,47 +40,52 @@ class SetUpYourShips : ComponentActivity() {
     fun TableCell(
         text: String
     ) {
-        val width: Int = resources.displayMetrics.widthPixels / 28
-        val height: Int = resources.displayMetrics.heightPixels / 28
-        Image(painter = painterResource(id = R.drawable.saltwater), contentDescription = "huhg",
+        Image(painter = painterResource(id = R.drawable.water), contentDescription = "huhg",
             contentScale= ContentScale.FillBounds,
             modifier = Modifier
-                .size(if(width<height) width.dp else height.dp)
+                .padding(1.dp)
+                .aspectRatio(1f)
+                .fillMaxWidth()
                 .clickable {
                     Toast
                         .makeText(
                             applicationContext,
                             text,
                             Toast.LENGTH_SHORT
-                        ).show()
+                        )
+                        .show()
                 }
-                .padding(1.dp),
+
+
         )
     }
     @Preview(showBackground = true)
     @Composable
     fun MainView() {
-        val tableData = (1..100).mapIndexed { index, item ->
-            index to "Item $index"
-        }
-        // Each cell of a column must have the same weight.
-
-        // The LazyColumn will be our table. Notice the use of the weights below
-        var count = 0
-        LazyColumn(
-            Modifier
-                .fillMaxSize()
-                .padding(16.dp)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            items(10) {
-                val id = it
-                LazyRow(Modifier.fillParentMaxWidth()) {
-                    items(10) {
-                        TableCell(text = (count).toString())
-                        count++
+            // Each cell of a column must have the same weight.
+            Box(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .aspectRatio(1f)
+
+            )
+            {
+                LazyVerticalGrid(
+                    userScrollEnabled = false,
+                    columns = GridCells.Fixed(10),
+                    content = {
+                        items(100) {
+                            TableCell("")
+
+                        }
                     }
-                }
+                )
             }
+
         }
+        // The LazyColumn will be our table. Notice the use of the weights below
     }
 }
